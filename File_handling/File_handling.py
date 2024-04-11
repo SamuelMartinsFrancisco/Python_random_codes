b26bc7f1452ef3077d371e4593d9293451bfc3cd
Samuel Martins Francisco <samuelmartinsfrancisco@gmail.com>
2024-04-11 17:04:26 -0300

    um novo exemplo de estudo foi escrito

--- File_handling/File_handling.py
+++ File_handling/File_handling.py
@@ -1,18 +1,37 @@
 
-def fileHandling(path):
+def readFile(path):
     file = open(path, 'r')
     content = file.read()
-    content = content.strip()
+    file.close()
     
-    if len(content) > 10:
-        print('No arquivo há mais de 10 letras, números, ou símbolos')
-        return content
+    return content
+
+def rewriteFile(path, content = str()):
+    file = open(path, 'w')
+    file.write(content)
+    file.close()
+
+def addToFile(path, content = str()):
+    file = open(path, 'a')
+    file.write(content)
+    file.close()
+
+def fileHandling(path):
+    fileLen = len(readFile(path))
+    print('Conteúdo inicial: ' + readFile(path) + '\n')
+    
+    if fileLen < 10:
+        print('O arquivo parece meio vazio, olhe só quantas teias de aranha!')
+        text = input('Pois então escreva mais algo nele: ')
+        addToFile(path, text)
+        print('\nBem melhor :D\n', readFile(path))
     else:
-        print('O arquivo está quase vazio!')
-        text = input('Escreva algo nele nesse caso: ')
-        file = open(path, 'w')
-        file.write(text)
-        file.close()
+        rewrite = input('Gostaria de reescrever o arquivo? (digite "s" ou "n")')
+        if rewrite == 's':
+            text = input('Pois então digite o novo conteúdo: ')
+            rewriteFile(path, text)
+            print('\nO arquivo foi reescrito!\n', readFile(path))
+            
 
 
 fileHandling('file.txt')
